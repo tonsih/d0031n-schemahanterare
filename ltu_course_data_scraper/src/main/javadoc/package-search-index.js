5b3a7b0bece2cc7f307a0e53fb4ab835af6bd375
Toni Sihvola <svenne.svensson@posteo.se>
2024-04-22 09:05:35 +0200

    Add JavaDoc files

---
+++ ltu_course_data_scraper/src/main/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"dev.toni.ltu_course_data_scraper"},{"l":"dev.toni.ltu_course_data_scraper.api"},{"l":"dev.toni.ltu_course_data_scraper.constants"},{"l":"dev.toni.ltu_course_data_scraper.gui"},{"l":"dev.toni.ltu_course_data_scraper.model"},{"l":"dev.toni.ltu_course_data_scraper.parser"}];updateSearchResults();
